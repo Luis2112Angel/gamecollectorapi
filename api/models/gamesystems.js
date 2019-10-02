7bcc463f896628cec9ccf715f075dcb11d66b998
Luis2112Angel <luis9925.la@gmail.com>
2019-10-02 17:53:51 -0500

    update

--- api/models/gamesystems.js
+++ api/models/gamesystems.js
@@ -1,7 +1,8 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Gamesystems = sequelize.define('Gamesystems', {
-    name: DataTypes.STRING
+    name: DataTypes.STRING,
+    description: DataTypes.STRING
   }, {});
   Gamesystems.associate = function(models) {
     // associations can be defined here
--- api/models/videogames.js
+++ api/models/videogames.js
@@ -1,7 +1,11 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Videogames = sequelize.define('Videogames', {
-    name: DataTypes.STRING
+    name: DataTypes.STRING,
+    developer: DataTypes.STRING,
+    gamesystem: DataTypes.STRING,
+    genre: DataTypes.STRING,
+    year: DataTypes.STRING
   }, {});
   Videogames.associate = function(models) {
     // associations can be defined here
